3b15b77b354151ca9b843a9d123a2245145d4ce3
DrGooseman <jamesjquinn12@gmail.com>
2020-12-22 22:40:01 +0100

    Add REST API for movies

--- Vidly/Dtos/MovieDto.cs
+++ Vidly/Dtos/MovieDto.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 
-namespace Vidly.Models
+namespace Vidly.Dtos
 {
-    public class Movie
+    public class MovieDto
     {
         public int Id { get; set; }
 
@@ -14,18 +14,11 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        public Genre Genre { get; set; }
-
-        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
-        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
-        public DateTime DateAdded { get; set; }
-
-        [Display(Name = "Number In Stock")]
-        [Range(1,20)]
+        [Range(1, 20)]
         public int NumberInStock { get; set; }
     }
 }
